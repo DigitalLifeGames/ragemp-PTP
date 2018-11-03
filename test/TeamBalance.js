197f0e920ad0cba3b3afca3c0adf571f50d6fa83
Joseph Peterson <josephpetersonn@gmail.com>
2018-11-03 11:36:10 -0500

    Team Balance Tests

--- test/TeamBalance.js
+++ test/TeamBalance.js
@@ -8,56 +8,48 @@ var preferences = require("../packages/ptp/configs/default.js");
 //Disable logging
 Console.log = () => {}
 describe('Team Balance Tests',() => {
+    
+    it('should not start, 1 player',function () {
 
-    describe('Game starts 5 players',() => {
-        it('should teams be even',function () {
+        var expected = [0,0,0,0,0];
 
-            var expected = [1,1,1,1,1];
-
-            let Game = new GameState({
-                teams : [{
-                    minPlayers: 1,
-                    maxPlayers: 1,
-                    name: "President",
-                    spawns: [new mp.Vector3(0,0,0)]
-                },{
-                    minPlayers: 0,
-                    maxPlayers: 1,
-                    name: "Vice President",
-                    spawns: [new mp.Vector3(0,0,0)]
-                },{
-                    minPlayers: 1,
-                    maxPlayers: false,
-                    name: "Terrorist",
-                    spawns: [new mp.Vector3(0,0,0)]
-                },{
-                    minPlayers: 0,
-                    name: "Security",
-                    spawns: [new mp.Vector3(0,0,0)]
-                },{
-                    minPlayers: 0,
-                    name: "Police",
-                    spawns: [new mp.Vector3(0,0,0)]
-                }]
-            });
-
-            Game.add(new mp.Player("Plornt"));
-            Game.add(new mp.Player("Schamens"));
-            Game.add(new mp.Player("Mercury"));
-            Game.add(new mp.Player("Tricky"));
-            Game.add(new mp.Player());
-
-            Game.teamBalance();
-
-            
-            var actual = [Game.teams[0].length,Game.teams[1].length,Game.teams[2].length,Game.teams[3].length,Game.teams[4].length];
-
-            true;
-            assert.deepEqual(actual,expected);
+        let Game = new GameState({
+            teams : [{
+                minPlayers: 1,
+                maxPlayers: 1,
+                name: "President",
+                spawns: [new mp.Vector3(0,0,0)]
+            },{
+                minPlayers: 0,
+                maxPlayers: 1,
+                name: "Vice President",
+                spawns: [new mp.Vector3(0,0,0)]
+            },{
+                minPlayers: 1,
+                maxPlayers: false,
+                name: "Terrorist",
+                spawns: [new mp.Vector3(0,0,0)]
+            },{
+                minPlayers: 0,
+                name: "Security",
+                spawns: [new mp.Vector3(0,0,0)]
+            },{
+                minPlayers: 0,
+                name: "Police",
+                spawns: [new mp.Vector3(0,0,0)]
+            }]
         });
+
+        Game.add(new mp.Player("Plornt"));
+
+        Game.teamBalance();
+
+        var teams = Game.teams;
+        var actual = [teams[0].length,teams[1].length,teams[2].length,teams[3].length,teams[4].length];
+
+        assert.deepEqual(actual,expected);
     });
 
-    
     describe('Game starts 2 players',() => {
         it('should teams be President, Terrorist',function () {
 
@@ -195,6 +187,54 @@ describe('Team Balance Tests',() => {
             var teams = Game.teams;
             var actual = [teams[0].length,teams[1].length,teams[2].length,teams[3].length,teams[4].length];
 
+            assert.deepEqual(actual,expected);
+        });
+    });
+
+    describe('Game starts 5 players',() => {
+        it('should teams be even',function () {
+
+            var expected = [1,1,1,1,1];
+
+            let Game = new GameState({
+                teams : [{
+                    minPlayers: 1,
+                    maxPlayers: 1,
+                    name: "President",
+                    spawns: [new mp.Vector3(0,0,0)]
+                },{
+                    minPlayers: 0,
+                    maxPlayers: 1,
+                    name: "Vice President",
+                    spawns: [new mp.Vector3(0,0,0)]
+                },{
+                    minPlayers: 1,
+                    maxPlayers: false,
+                    name: "Terrorist",
+                    spawns: [new mp.Vector3(0,0,0)]
+                },{
+                    minPlayers: 0,
+                    name: "Security",
+                    spawns: [new mp.Vector3(0,0,0)]
+                },{
+                    minPlayers: 0,
+                    name: "Police",
+                    spawns: [new mp.Vector3(0,0,0)]
+                }]
+            });
+
+            Game.add(new mp.Player("Plornt"));
+            Game.add(new mp.Player("Schamens"));
+            Game.add(new mp.Player("Mercury"));
+            Game.add(new mp.Player("Tricky"));
+            Game.add(new mp.Player());
+
+            Game.teamBalance();
+
+            
+            var actual = [Game.teams[0].length,Game.teams[1].length,Game.teams[2].length,Game.teams[3].length,Game.teams[4].length];
+
+            true;
             assert.deepEqual(actual,expected);
         });
     });
