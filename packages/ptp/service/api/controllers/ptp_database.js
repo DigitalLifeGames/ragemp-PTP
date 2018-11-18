e7c8161ad23d268376c541ca3310a28adea4ddbc
josephpeterson <josephpetersonn@gmail.com>
2018-11-18 12:27:30 -0600

    Added Database Admin list end-point to PTPService

--- packages/ptp/service/api/controllers/ptp_database.js
+++ packages/ptp/service/api/controllers/ptp_database.js
@@ -28,6 +28,37 @@ function Leaderboard(req,res)
         res.send(rows);
     });
 }
+/**
+ * @swagger
+ * /db/admins:
+ *   get:
+ *     tags:
+ *       - Database Service
+ *     description: Returns list of admins 
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Array of Player objects
+ *         schema:
+ *           $ref: '#/definitions/Player'
+ */
+function Admins(req,res)
+{
+    var game = req.app.get("service").game;
+    var db = game.database();
+    if(!db)
+    {
+        res.send({
+            error: "No database available"
+        });
+        return;
+    }
+    db.select('Admins').then(rows => {
+        res.send(rows);
+    });
+}
 module.exports = function(app) {
     app.route('/db/board').get(Leaderboard);
+    app.route('/db/admins').get(Admins);
 };
